452128adea4c52b6e1578c4d3480d0c36627cd2b
yebz <yebz@qhfct.com>
2021-01-08 16:27:53 +0800

    移动端适配，修复日期bug

--- server/api.go
+++ server/api.go
@@ -9,7 +9,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var weekDayS = []string{"一", "二", "三", "四", "五", "六", "日"}
+var weekDayS = []string{"日", "一", "二", "三", "四", "五", "六"}
 
 func GetDate(ctx *gin.Context) {
 	now := time.Now()
